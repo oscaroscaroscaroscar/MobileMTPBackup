d3b34590ad71a0edfd0709d2b5756f0013434f1b
oscaroscaroscaroscar <oscaraberg@gmail.com>
2026-09-06 15:48:52 +0200

    v4.9 simplify app startup for first runnable build

--- MobileMTPBackup/App.xaml.cs
+++ MobileMTPBackup/App.xaml.cs
@@ -1,27 +1,7 @@
-using System.Windows.Threading;
-using MobileMTPBackup.Services;
 using System.Windows;
 
 namespace MobileMTPBackup;
 
 public partial class App : Application
 {
-    public App()
-    {
-        DispatcherUnhandledException += (_, e) =>
-        {
-            try
-            {
-                string path = CrashLogger.Log(e.Exception);
-                MessageBox.Show(
-                    $"Ett oväntat fel inträffade. En kraschlogg sparades här:\n{path}",
-                    "Mobile MTP Backup",
-                    MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-            }
-            catch { }
-
-            e.Handled = true;
-        };
-    }
 }
