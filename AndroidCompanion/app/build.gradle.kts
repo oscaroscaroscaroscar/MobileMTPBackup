918b20d6a1d9daab5c71fd210ea67951225f900a
oscaroscaroscaroscar <oscaraberg@gmail.com>
2026-09-13 13:57:29 +0200

    Companion 0.12 version bump

--- AndroidCompanion/app/build.gradle.kts
+++ AndroidCompanion/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "se.oscar.mobilemtpbackup.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.11.0"
+        versionCode = 12
+        versionName = "0.12.0"
     }
 
     compileOptions {
